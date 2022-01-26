8026d0a2d150ca69ec59fa9076bb7804e56f6332
Tushar <tm2582001@gmail.com>
2022-01-26 17:39:07 +0530

    added style for result page and do some fixes

--- javaScript/result.js
+++ javaScript/result.js
@@ -3,5 +3,13 @@ let classes = (classes) => document.getElementsByClassName(classes);
 
 window.addEventListener('load', () => {
     let questionAnswersGiven =  sessionStorage.getItem("questionanswersGiven");
-    id("result").innerHTML =questionAnswersGiven;
+    id("result").innerHTML =  `${questionAnswersGiven}/5`;
+    if(questionAnswersGiven<=2){
+        id("feedback").innerHTML = "UGH!! you need improvement";
+    }else if(questionAnswersGiven === 3){
+        id("feedback").innerHTML = "You are almost there try again";
+    }else{
+        id("feedback").innerHTML = "Congratulations You made it";
+
+    }
 });
